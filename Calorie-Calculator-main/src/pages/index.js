9debf31ad8ee1629be98a9630f7700437414fb4c
wayzedgolden1 <131336883+wayzedgolden1@users.noreply.github.com>
2025-11-12 06:49:02 +0700

    Delete Calorie-Calculator-main directory

--- Calorie-Calculator-main/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import {CalorieCalculatorPage} from "@/components/calorie-calculator";
-
-export default function Home() {
-  return (
-    <main>
-      <CalorieCalculatorPage />
-    </main>
-  );
-}
